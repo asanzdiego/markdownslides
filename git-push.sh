1ec4b81172d6d09fa94457bf6aec727766d7a1b1
asanzdiego <asanzdiego@gmail.com>
2014-01-15 10:32:47 +0100

    first release

---
+++ git-push.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "**************************"
+echo "* PUSH CHANGES TO GITHUB *"
+echo "**************************"
+
+git diff | grep +++
+
+read -p "You want to continue? [y|*N*]: " OPTION
+
+if [ "$OPTION" == "y" ]; then
+
+    read -p "Write the commit message: " MESSAGE
+
+    git add . && \
+    git commit -m "$MESSAGE" && \
+    git push
+fi
